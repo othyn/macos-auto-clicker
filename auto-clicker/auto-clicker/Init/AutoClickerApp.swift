33f0a397b853c34666f065248c3ae08d6e249e88
Ben <git@othyn.com>
2022-03-30 17:08:27 +0100

    Add customisable global keyboard shortcuts and clean up prefs/menu bar items

--- auto-clicker/auto-clicker/Init/AutoClickerApp.swift
+++ auto-clicker/auto-clicker/Init/AutoClickerApp.swift
@@ -7,43 +7,6 @@
 
 import Foundation
 import SwiftUI
-import Cocoa
-
-final class AppDelegate: NSObject, NSApplicationDelegate {
-    func applicationDidFinishLaunching(_ notification: Notification) {
-        if let window = NSApplication.shared.mainWindow {
-            window.titlebarAppearsTransparent = true
-
-            let customToolbar = NSToolbar()
-            customToolbar.showsBaselineSeparator = false
-            window.toolbar = customToolbar
-        }
-    }
-    
-    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-        return true
-    }
-    
-    func applicationWillUpdate(_ notification: Notification) {
-        if let menu = NSApplication.shared.mainMenu {
-            if let file = menu.items.first(where: { $0.title == "File"}) {
-                menu.removeItem(file);
-            }
-            if let edit = menu.items.first(where: { $0.title == "Edit"}) {
-                menu.removeItem(edit);
-            }
-            if let window = menu.items.first(where: { $0.title == "Window"}) {
-                menu.removeItem(window);
-            }
-            if let view = menu.items.first(where: { $0.title == "View"}) {
-                menu.removeItem(view);
-            }
-            if let help = menu.items.first(where: { $0.title == "Help"}) {
-                menu.removeItem(help);
-            }
-        }
-    }
-}
 
 @main
 struct AutoClickerApp: App {
@@ -54,9 +17,9 @@ struct AutoClickerApp: App {
     @State private var keepWindowOnTop: Bool = false
 
     var body: some Scene {
-//        Settings {
-//            EmptyView()
-//        }
+        Settings {
+            SettingsView()
+        }
         
         WindowGroup {
             ZStack {
@@ -70,10 +33,7 @@ struct AutoClickerApp: App {
         }
         .windowStyle(.hiddenTitleBar)
         .commands {
-            CommandMenu("Options") {
-                Toggle("Keep window on top", isOn: self.$keepWindowOnTop)
-                    .keyboardShortcut("k")
-            }
+            OptionsCommands(keepWindowOnTop: self.$keepWindowOnTop)
         }
         .onChange(of: self.keepWindowOnTop) { isOn in
             WindowStateService.toggleKeepWindowOnTop(isOn)
