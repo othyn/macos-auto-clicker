cdde35a404f9409d26a6079669864857a40f8497
Ben <git@othyn.com>
2022-07-06 17:02:04 +0100

    Remove arbitrary limits on custom fields

--- auto-clicker/Constants/FieldConstants.swift
+++ auto-clicker/Constants/FieldConstants.swift
@@ -12,14 +12,14 @@ import Foundation
 // MARK: - Press Interval
 
 let MIN_PRESS_INTERVAL: Int = 1
-let MAX_PRESS_INTERVAL: Int = 1_000
+let MAX_PRESS_INTERVAL: Int = 100_000_000
 
 let DEFAULT_PRESS_INTERVAL: Int = 50
 
 // MARK: - Press Amount
 
 let MIN_PRESS_AMOUNT: Int = 1
-let MAX_PRESS_AMOUNT: Int = 100
+let MAX_PRESS_AMOUNT: Int = 100_000_000
 
 let DEFAULT_PRESS_AMOUNT: Int = 1
 
@@ -33,6 +33,6 @@ let DEFAULT_REPEAT_AMOUNT: Int = 100
 // MARK: - Start Delay
 
 let MIN_START_DELAY: Int = 0
-let MAX_START_DELAY: Int = 60
+let MAX_START_DELAY: Int = 100_000_000
 
 let DEFAULT_START_DELAY: Int = 1
