8afca88625055e80ac246c95fb31704ae3517071
Ben <git@othyn.com>
2022-07-16 16:08:11 +0200

    chore: Playing around with the menu bar icon, this appears to add the icon without any functionality

--- auto-clicker/Init/AppDelegate.swift
+++ auto-clicker/Init/AppDelegate.swift
@@ -9,6 +9,8 @@ import Foundation
 import Cocoa
 
 final class AppDelegate: NSObject, NSApplicationDelegate {
+    private var statusItem: NSStatusItem!
+
     func applicationDidFinishLaunching(_ notification: Notification) {
         if let window = NSApplication.shared.mainWindow {
             window.titlebarAppearsTransparent = true
@@ -16,6 +18,12 @@ final class AppDelegate: NSObject, NSApplicationDelegate {
             let customToolbar = NSToolbar()
             customToolbar.showsBaselineSeparator = false
             window.toolbar = customToolbar
+        }
+
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+
+        if let button = statusItem.button {
+            button.image = NSImage(systemSymbolName: "cursorarrow.click.badge.clock", accessibilityDescription: "auto clicker")
         }
 
         PermissionsService.acquireAccessibilityPrivileges()
