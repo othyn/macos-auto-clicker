18d170b48114b8bc6b147d9dc3f2b1361ea49ef9
Ben <git@othyn.com>
2022-07-18 16:55:06 +0200

    chore: Weirdly solved the issue I was having in commit bca4d72d1abb0b284149378ab9b63544514ca7f8, in regards to issue #44, whilst implementing the functionality to restore the main window from the menu bar as part of the 'minimize to menu bar' functionality, so the window size state is now persisted correctly... ish.

--- auto-clicker/Init/AppDelegate.swift
+++ auto-clicker/Init/AppDelegate.swift
@@ -8,7 +8,7 @@
 import Foundation
 import Cocoa
 
-final class AppDelegate: NSObject, NSApplicationDelegate {
+final class AppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate {
     func applicationDidFinishLaunching(_ notification: Notification) {
         if let window = NSApplication.shared.mainWindow {
             window.titlebarAppearsTransparent = true
@@ -17,6 +17,11 @@ final class AppDelegate: NSObject, NSApplicationDelegate {
             customToolbar.showsBaselineSeparator = false
             window.toolbar = customToolbar
         }
+
+        // Hacky workaround in SwiftUI in order to have macOS persist the window size state
+        // https://stackoverflow.com/a/72558375/4494375
+        let mainWindow = NSApp.windows[0]
+        mainWindow.delegate = self
 
         MenuBarService.refreshState()
 
@@ -29,5 +34,13 @@ final class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationWillBecomeActive(_ notification: Notification) {
         WindowStateService.refreshKeepWindowOnTop()
+    }
+
+    // Hacky workaround in SwiftUI in order to have macOS persist the window size state
+    // https://stackoverflow.com/a/72558375/4494375
+    func windowShouldClose(_ sender: NSWindow) -> Bool {
+        NSApp.hide(nil)
+
+        return false
     }
 }
